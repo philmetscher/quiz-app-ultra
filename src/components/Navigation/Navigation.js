30ac81732a2fad74f12cf2ea9badd457cbf1b765
Phil Metscher <philmetschermailt@gmx.de>
2022-09-19 16:21:37 +0200

    replace class with className

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,29 +2,29 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <nav class="nav-main">
-      <ul class="nav__list">
-        <li class="nav__entry nav__entry--active">
-          <a href="#" class="nav__link">
-            <i class="fa-solid fa-house"></i>
+    <nav className="nav-main">
+      <ul className="nav__list">
+        <li className="nav__entry nav__entry--active">
+          <a href="#" className="nav__link">
+            <i className="fa-solid fa-house"></i>
             <span>Home</span>
           </a>
         </li>
-        <li class="nav__entry">
-          <a href="./bookmarks.html" class="nav__link">
-            <i class="fa-solid fa-bookmark"></i>
+        <li className="nav__entry">
+          <a href="./bookmarks.html" className="nav__link">
+            <i className="fa-solid fa-bookmark"></i>
             <span>Merkliste</span>
           </a>
         </li>
-        <li class="nav__entry">
-          <a href="./form.html" class="nav__link">
-            <i class="fa-solid fa-circle-plus"></i>
+        <li className="nav__entry">
+          <a href="./form.html" className="nav__link">
+            <i className="fa-solid fa-circle-plus"></i>
             <span>neue Frage</span>
           </a>
         </li>
-        <li class="nav__entry">
-          <a href="./profile.html" class="nav__link">
-            <i class="fa-solid fa-user"></i>
+        <li className="nav__entry">
+          <a href="./profile.html" className="nav__link">
+            <i className="fa-solid fa-user"></i>
             <span>Profile</span>
           </a>
         </li>
