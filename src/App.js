9b3f9f359d2b31cb95ce186d6380d7dc7d021c5b
Phil Metscher <philmetschermailt@gmx.de>
2022-09-20 16:18:33 +0200

    add db.js for each card

--- src/App.js
+++ src/App.js
@@ -3,30 +3,7 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Card from "./components/card/Card";
 import Navigation from "./components/navigation/Navigation";
-
-const cards = [
-  {
-    id: 0,
-    question:
-      "In the Kingdom Heart series who provides the english voice for Master Eraqus?",
-    answer: "Mark Hamill",
-    tags: ["kingdomheart", "mastereraqus", "english voices"],
-  },
-  {
-    id: 1,
-    question:
-      "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
-    answer: "Harry Potter",
-    tags: ["danielradcliffe", "hewhomustnotbenamed", "abracadabra"],
-  },
-  {
-    id: 2,
-    question:
-      'Which former US president was nicknamed "Teddy" after he refused to shoot a defenseless black bear?',
-    answer: "Theodore Roosevelt",
-    tags: ["usa", "president", "teddybear"],
-  },
-];
+import { cards } from "./db";
 
 function App() {
   return (
---
+++ src/db.js
@@ -0,0 +1,23 @@
+export const cards = [
+  {
+    id: 0,
+    question:
+      "In the Kingdom Heart series who provides the english voice for Master Eraqus?",
+    answer: "Mark Hamill",
+    tags: ["kingdomheart", "mastereraqus", "english voices"],
+  },
+  {
+    id: 1,
+    question:
+      "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
+    answer: "Harry Potter",
+    tags: ["danielradcliffe", "hewhomustnotbenamed", "abracadabra"],
+  },
+  {
+    id: 2,
+    question:
+      'Which former US president was nicknamed "Teddy" after he refused to shoot a defenseless black bear?',
+    answer: "Theodore Roosevelt",
+    tags: ["usa", "president", "teddybear"],
+  },
+];
