688f6f2850a2443629025807a7304d7504281973
Phil Metscher <philmetschermailt@gmx.de>
2022-09-22 09:59:35 +0200

    add Form html & styling

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,5 +1,48 @@
+import "./Form.css";
+
 function Form() {
-  return <p>Under construction!</p>;
+  return (
+    <form class="new-question" data-js="questionForm">
+      <div class="form-group">
+        <textarea
+          name="question"
+          id="question"
+          cols="30"
+          rows="4"
+          placeholder="Your Question:"
+          autoFocus
+          autocomplete="off"
+        ></textarea>
+        <label for="question">Your Question:</label>
+      </div>
+      <div class="form-group">
+        <textarea
+          name="answer"
+          id="answer"
+          cols="30"
+          rows="4"
+          placeholder="Your Answer:"
+          autocomplete="off"
+        ></textarea>
+        <label for="answer">Your Answer:</label>
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          name="tags"
+          id="tags"
+          placeholder="Tags:"
+          autocomplete="off"
+        />
+        <label for="tags">
+          Tags: <small>(seperated with ",")</small>
+        </label>
+      </div>
+      <button class="new-question__submit" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default Form;
